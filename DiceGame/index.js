0953773ab3e692198cc66414c261d55e7de83944
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-04-23 13:54:14 -0700

    Done

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,12 +1,21 @@
 //alert("Working");
 
-var ran = Math.floor((Math.random()* 6)+ 1);
+var ran1 = Math.floor((Math.random()* 6)+ 1);
+var ran2 = Math.floor((Math.random()* 6)+ 1);
 
-document.querySelector(".img1").setAttribute("src","./images/dice"+ran+".png");
 
-var ran = Math.floor((Math.random()* 6)+ 1);
+document.querySelector(".img1").setAttribute("src","./images/dice"+ran1+".png");
 
-document.querySelector(".img2").setAttribute("src","./images/dice"+ran+".png");
 
+document.querySelector(".img2").setAttribute("src","./images/dice"+ran2+".png");
+
+if(ran1 === ran2){
+    document.querySelector("h1").innerHTML  = "Draw";
+}else if(ran1 > ran2){
+    document.querySelector("h1").innerHTML  = "Player 1 won";
+}
+else{
+    document.querySelector("h1").innerHTML  = "Player 2 won";
+}
 
 console.log(ran);
