98c7e508d499e89d25f3ece97319ccca2d65b684
Hamza-Nayab <Hamza.nayab48@gmail.com>
2024-04-23 13:40:24 -0700

    Added random

---
+++ DiceGame/index.js
@@ -0,0 +1,12 @@
+//alert("Working");
+
+var ran = Math.floor((Math.random()* 6)+ 1);
+
+document.querySelector(".img1").setAttribute("src","./images/dice"+ran+".png");
+
+var ran = Math.floor((Math.random()* 6)+ 1);
+
+document.querySelector(".img2").setAttribute("src","./images/dice"+ran+".png");
+
+
+console.log(ran);
